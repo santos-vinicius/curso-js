d1df0022b7a8b234442b229aeb4a1d64c1175309
Vinicius Santos <santosviniv@gmail.com>
2022-08-17 14:05:32 -0300

    JS-Basico: Exercício função, array e objeto

--- JS-Básico/aula19/script.js
+++ JS-Básico/aula19/script.js
@@ -13,7 +13,18 @@ function meuEscopo() {
     const peso = form.querySelector('.peso');
     const altura = form.querySelector('.altura');
 
+    pessoas.push({
+      nome: nome.value,
+      sobrenome: sobrenome.value,
+      peso: peso.value,
+      altura: altura.value
+    });
+
     console.log(pessoas);
+
+    resultado.innerHTML +=
+      `<p>${nome.value} ${sobrenome.value} ` +
+      `${peso.value} ${altura.value}</p>`;
   }
   form.addEventListener('submit', recebeEventoForm);
 }
