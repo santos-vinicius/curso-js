5ee1d8af8ce5d185dabb7e91e60ccb78ed232bea
Vinicius Santos <santosviniv@gmail.com>
2022-08-29 17:56:28 -0300

    JS-Lógica: Aulas 39-39

---
+++ JS-Lógica/aula37/assets/js/main.js
@@ -0,0 +1,11 @@
+const paragrafos = document.querySelector('.paragrafos');
+const ps = paragrafos.querySelectorAll('p');
+
+const estilosBody = getComputedStyle(document.body);
+const backgroudColorBody = estilosBody.backgroundColor;
+console.log(backgroudColorBody);
+
+for (let p of ps) {
+  p.style.backgroundColor = backgroudColorBody;
+  p.style.color = '#FFFFFF';
+}
---
+++ JS-Lógica/aula38/aula38.js
@@ -0,0 +1,29 @@
+// While e Do While -  Estrutura de Repetição
+
+// let controle = 0;
+
+// while (controle <= 10) {
+//   console.log(controle);
+//   controle++;
+// }
+
+function random(min, max) {
+  const r = Math.random() * (max - min) + min;
+  return Math.floor(r);
+}
+
+const min = 1;
+const max = 50;
+let rand = 10;
+
+while (rand !== 10) {
+  rand = random(min, max);
+  console.log(rand);
+}
+
+console.log('#######');
+
+do {
+  rand = random(min, max);
+  console.log(rand);
+} while (rand !== 10);
