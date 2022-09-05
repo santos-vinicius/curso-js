e8a36d7078b857724afc2cc237e41efe5d3184c6
Vinicius Santos <santosviniv@gmail.com>
2022-09-05 13:20:15 -0300

    JS-Lógica: Aula 47(WIP)

---
+++ JS-Lógica/aula47/assets/js/main.js
@@ -0,0 +1,49 @@
+const inputTarefa = document.querySelector('.input-tarefa');
+const bntTarefa = document.querySelector('.btn-tarefa');
+const tarefas = document.querySelector('.tarefas');
+
+function criaLi() {
+  const li = document.createElement('li');
+  return li;
+}
+
+inputTarefa.addEventListener('keypress', function (e) {
+  if (e.keyCode === 13) {
+    if (!inputTarefa.value) return;
+    criaTarefa(inputTarefa.value);
+  }
+});
+
+function limpaInput() {
+  inputTarefa = '';
+  inputTarefa.focus();
+}
+
+function criaBotaoApagar(li) {
+  li.innerText += ' ';
+  const botaoApagar = document.createElement('button');
+  botaoApagar.innerText = 'Apagar';
+  botaoApagar.setAttribute('class', 'apagar');
+  botaoApagar.setAttribute('title', 'Apagar esta tarefa.');
+  li.appendChild(botaoApagar);
+}
+
+function criaTarefa(textoInput) {
+  const li = criaLi();
+  li.innerText = textoInput;
+  tarefas.appendChild(li);
+  criaBotaoApagar(li);
+  limpaInput();
+}
+
+bntTarefa.addEventListener('click', function (e) {
+  if (!inputTarefa.value) return;
+  criaTarefa(inputTarefa.value);
+});
+
+document.addEventListener('click', function (e) {
+  const el = e.target;
+  if (el.classList.contains('apagar')) {
+    console.log('apagar clicado');
+  }
+});
