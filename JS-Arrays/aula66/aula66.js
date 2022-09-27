366c3613dba7c72c9e4eff112d07a5ced5a09f8e
Vinicius Santos <santosviniv@gmail.com>
2022-09-27 01:31:31 -0300

    JS-Arrays: Aulas 66-68

--- JS-Arrays/aula66/aula66.js
+++ JS-Arrays/aula66/aula66.js
@@ -2,8 +2,6 @@
 
 // Some todos os números (reduce)
 // Retorne um array com os pares (Filter)
-// Retorne um array com o dobro dos valores (Map)
-
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
 const total = numeros.reduce(function (acumulador, valor, indice, array) {
   acumulador += valor;
@@ -16,3 +14,20 @@ const pares = numeros.reduce(function (acumulador, valor) {
   return acumulador;
 }, []);
 console.log(pares);
+
+// retorne a pessoa mais velha
+
+const pessoas = [
+  { nome: 'Luiz', idade: 62 },
+  { nome: 'Maria', idade: 23 },
+  { nome: 'Eduardo', idade: 55 },
+  { nome: 'Letícia', idade: 10 },
+  { nome: 'Rosana', idade: 64 },
+  { nome: 'Wallace', idade: 63 }
+];
+
+const maisVelha = pessoas.reduce(function (acumulador, valor) {
+  if (acumulador.idade > valor.idade) return acumulador;
+  return valor;
+});
+console.log(maisVelha);
---
+++ JS-Arrays/aula68/aula68.js
@@ -0,0 +1,8 @@
+// For each
+
+const a1 = [10, 20, 30, 40, 50];
+let total = 0;
+a1.forEach(valor => {
+  total += valor;
+});
+console.log(total);
