aa895818cd8fb0a535bded680281b5036c491425
Vinicius Santos <santosviniv@gmail.com>
2022-09-30 14:14:41 -0300

    JS-Classes: Aula 83

---
+++ JS-Classes/aula83/aula83.js
@@ -0,0 +1,25 @@
+class ControleRemoto {
+  constructor(tv) {
+    this.tv = tv;
+    this.volume = 0;
+  }
+
+  // Método de instância
+
+  aumentarVolume() {
+    this.volume += 2;
+  }
+  diminuirVolume() {
+    this.volume -= 2;
+  }
+
+  // Método estático
+  static trocaPilha() {
+    console.log('Ok, vou trocar a pilha.');
+  }
+}
+
+const controle1 = new ControleRemoto('LG');
+controle1.aumentarVolume();
+ControleRemoto.trocaPilha();
+console.log(controle1);
