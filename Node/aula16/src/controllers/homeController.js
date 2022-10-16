3b766d7b0155f5c48e8153317f94b8a3cd4919b1
Vinicius Santos <37769587+santos-vinicius@users.noreply.github.com>
2022-10-16 00:31:01 -0300

    Node: Aula 15

---
+++ Node/aula16/src/controllers/homeController.js
@@ -0,0 +1,12 @@
+exports.paginaInicial = (req, res) => {
+  res.render('index', {
+    titulo: 'Este será o titulo da página',
+    numeros: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  });
+  return;
+};
+
+exports.trataPost = (req, res) => {
+  //res.send('Formulário recebido!');
+  return;
+};
