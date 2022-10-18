1398dda523f0d53f0dab97b84cb44c9bbbf9c951
Vinicius Santos <37769587+santos-vinicius@users.noreply.github.com>
2022-10-18 05:16:03 -0300

    Node: fix Aula 16

--- Node/aula16/src/middlewares/middleware.js
+++ Node/aula16/src/middlewares/middleware.js
@@ -8,7 +8,7 @@ exports.outroMiddleware = (req, res, next) => {
 };
 
 exports.checkCsrfError = (err, req, res, next) => {
-  if (err && 'EBADCSFRTOKEN' === err.code) {
+  if (err && 'EBADCSRFTOKEN' === err.code) {
     return res.render('404');
   }
 };
