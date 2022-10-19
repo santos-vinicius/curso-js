3e46f370968ddf8e6c8c75e4fade1a727bf6dd4f
Vinicius Santos <37769587+santos-vinicius@users.noreply.github.com>
2022-10-18 22:55:57 -0300

    ProjetoAgenda: alertas de registro de usuário (wip)

--- Projetos/projetoagenda/src/controllers/loginController.js
+++ Projetos/projetoagenda/src/controllers/loginController.js
@@ -5,16 +5,26 @@ exports.index = (req, res) => {
 };
 
 exports.register = async function (req, res) {
-  const login = new Login(req.body);
-  await login.register();
+  try {
+    const login = new Login(req.body);
+    await login.register();
+  
+    if (login.errors.length > 0) {
+      req.flash('errors', login.errors);
+      req.session.save(function () {
+        return res.redirect('index');
+      });
+      return;
+    }
 
-  if (login.errors.length > 0) {
-    req.flash('errors', login.errors);
+    req.flash('success', 'Seu usuário foi criado com sucesso!');
     req.session.save(function () {
       return res.redirect('index');
     });
-    return;
-  }
 
-  res.send(login.errors);
+  } catch (e) {
+    console.log(e);
+    return res.render('404');
+  }
+  
 };
