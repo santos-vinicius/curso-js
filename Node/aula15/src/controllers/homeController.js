4baaca73d757f2fb0b1162b4c2d5b2a74980b936
Vinicius Santos <37769587+santos-vinicius@users.noreply.github.com>
2022-10-26 19:56:39 -0300

    Finalização do Projeto Agenda

--- Node/aula15/src/controllers/homeController.js
+++ Node/aula15/src/controllers/homeController.js
@@ -1,8 +1,8 @@
 exports.paginaInicial = (req, res) => {
-  req.flash('info', 'Olá mundo!');
-  req.flash('error', 'Erro!');
-  req.flash('success', 'Sucesso!');
-  res.render('index');
+  res.render('index', {
+    titulo: 'Este será o titulo da página',
+    numeros: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  });
   return;
 };
 
