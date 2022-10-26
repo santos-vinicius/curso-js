4720ad7d69765f57ebe4829cffcd596609269dbd
Vinicius Santos <37769587+santos-vinicius@users.noreply.github.com>
2022-10-25 22:38:18 -0300

    Projeto Agenda: Finalização do login

--- Projetos/projetoagenda/src/controllers/loginController.js
+++ Projetos/projetoagenda/src/controllers/loginController.js
@@ -1,14 +1,20 @@
 const Login = require('../models/LoginModel');
 
 exports.index = (req, res) => {
-  res.render('login');
+  if (req.session.user) {
+    return res.render('login-logado');
+  } else {
+    res.render('login');
+  }
 };
+
+// Registro de usuário
 
 exports.register = async function (req, res) {
   try {
     const login = new Login(req.body);
     await login.register();
-  
+
     if (login.errors.length > 0) {
       req.flash('errors', login.errors);
       req.session.save(function () {
@@ -21,10 +27,38 @@ exports.register = async function (req, res) {
     req.session.save(function () {
       return res.redirect('index');
     });
-
   } catch (e) {
     console.log(e);
-    return res.render('404');
+    return res.redirect('404');
   }
-  
+};
+
+// Login de Usuário
+exports.login = async function (req, res) {
+  try {
+    const login = new Login(req.body);
+    await login.login();
+
+    if (login.errors.length > 0) {
+      req.flash('errors', login.errors);
+      req.session.save(function () {
+        return res.redirect('index');
+      });
+      return;
+    }
+
+    req.flash('success', 'Login efetuado com sucesso!');
+    req.session.user = login.user;
+    req.session.save(function () {
+      return res.redirect('index');
+    });
+  } catch (e) {
+    console.log(e);
+    return res.redirect('404');
+  }
+};
+
+exports.logout = function (req, res) {
+  req.session.destroy();
+  res.redirect('index');
 };
--- Projetos/projetoagenda/src/middlewares/middleware.js
+++ Projetos/projetoagenda/src/middlewares/middleware.js
@@ -2,6 +2,7 @@ exports.middlewareGlobal = (req, res, next) => {
   res.locals.errors = req.flash('errors');
   res.locals.success = req.flash('success');
   next();
+  res.locals.user = req.session.user;
 };
 
 exports.outroMiddleware = (req, res, next) => {
