0c47824de131befd3fa6e83d7704de6f482d83cc
Vinicius Santos <santosviniv@gmail.com>
2022-10-26 23:58:59 -0300

    fix: removi comentário

--- Projetos/Agenda/frontend/main.js
+++ Projetos/Agenda/frontend/main.js
@@ -7,5 +7,5 @@ const login = new Login('.form-login');
 const cadastro = new Login('.form-cadastro');
 login.init();
 cadastro.init();
-// import './assets/css/style.css';
+// import './assets/css/style.css'
 
