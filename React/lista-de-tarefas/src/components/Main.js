4f92b81d6f2938fa7c445a2b6bfff9003c73c1e2
Vinicius <santosviniv@gmail.com>
2022-11-10 03:21:04 -0300

    React: Básico (wip)

---
+++ React/lista-de-tarefas/src/components/Main.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './Main.css';
+
+// Form
+import { FaPlus } from 'react-icons/fa';
+
+export default class Main extends Component {
+  state = {
+    novaTarefa: '',
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      novaTarefa: e.target.value,
+    });
+  };
+
+  render() {
+    const { novaTarefa } = this.state;
+
+    return (
+      <div className="main">
+        <h1>Lista de tarefas</h1>
+
+        <form action="#" className="form">
+          <input value={novaTarefa} onChange={this.handleChange} type="text" />
+          <button type="submit">
+            <FaPlus />
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
