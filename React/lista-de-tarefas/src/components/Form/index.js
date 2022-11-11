da14e217648ea4e140aff56892abef960e4e81dc
Vinicius <santosviniv@gmail.com>
2022-11-11 04:08:49 -0300

    react: finalização da aplicação

---
+++ React/lista-de-tarefas/src/components/Form/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FaPlus } from 'react-icons/fa';
+
+import './Form.css';
+
+export default function Form({ handleChange, handleSubmit, novaTarefa }) {
+  return (
+    <form onSubmit={handleSubmit} action="#" className="form">
+      <input value={novaTarefa} onChange={handleChange} type="text" />
+      <button type="submit">
+        <FaPlus />
+      </button>
+    </form>
+  );
+}
+
+Form.propTypes = {
+  handleChange: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  novaTarefa: PropTypes.string.isRequired,
+};
