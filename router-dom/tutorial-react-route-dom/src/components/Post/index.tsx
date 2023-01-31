a3ab85f431e2ebbb9e8d9f0a60696712973a88f8
Vinicius <santosviniv@gmail.com>
2023-01-31 14:27:10 -0300

    react router dom v6

---
+++ router-dom/tutorial-react-route-dom/src/components/Post/index.tsx
@@ -0,0 +1,10 @@
+import './style.css';
+
+export const Post = () => {
+  return (
+    <div>
+      <h1>Post</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab vitae quia, distinctio, consequuntur porro molestiae amet quisquam, molestias totam debitis eos qui. Similique consectetur officia minima consequatur possimus nulla iste.</p>
+    </div>
+  );
+};
